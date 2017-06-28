43d60d32af6e023dd1fa2606a2450bad7f215a89
ShoshieLan <shoshielan@gmail.com>
2017-06-28 10:10:15 -0400

    made if else for json and html

--- src/main/java/trial.java
+++ src/main/java/trial.java
@@ -9,6 +9,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.io.IOException;
@@ -26,9 +27,7 @@ public class trial {
 
     public static void main(String args[]) throws Exception {
 
-
-
-
+        
         String layout = "templates/layout.vtl";
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -41,87 +40,6 @@ public class trial {
             return  new ModelAndView(maps, layout);
         }, new VelocityTemplateEngine());
 
-
-
-        get("/event/json", (req, res) -> {
-                    ArrayList<Event> eventList = new ArrayList<>();
-
-                    Map<String, Object> model = new HashMap<>();
-
-                    String id = req.queryParams("id");
-
-
-                    HttpGet httpget = new HttpGet("http://oorah-admire04:9200/newindex/_search?q=KadonID:" + id + "");
-
-                    ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
-
-                        @Override
-                        public String handleResponse(
-                                final HttpResponse response) throws ClientProtocolException, IOException {
-                            int status = response.getStatusLine().getStatusCode();
-                            if (status >= 200 && status < 300) {
-                                HttpEntity entity = response.getEntity();
-                                return entity != null ? EntityUtils.toString(entity) : null;
-                            } else {
-                                throw new ClientProtocolException("Unexpected response status: " + status);
-                            }
-                        }
-
-                    };
-                    final JsonNode arrNode = new ObjectMapper().readTree(httpclient.execute(httpget, responseHandler)).get("hits").get("hits");
-
-                    Event event = new Event();
-
-                    if (arrNode.isArray()) {
-
-                        for (final JsonNode objNode : arrNode) {
-
-                            event = new EventBuilder().setKadonId(Integer.parseInt(id)).createEvent();
-                            if (objNode.get("_source").get("@timestamp") != null) {
-                                String Gettext = String.valueOf(objNode.get("_source").get("@timestamp").asText());
-                                Date date = formatDate(Gettext);
-                                event.setDateTime(date);
-                            }
-                            if (objNode.get("_source").get("EventName") != null) {
-                                String eventName = ("Event Name: " + objNode.get("_source").get("EventName").asText());
-                                event.setEventName(eventName);
-                            }
-
-                            if (objNode.get("_source").get("NewValue") != null) {
-                                String newValue = ("New Value: " + objNode.get("_source").get("NewValue").asText());
-                                event.setNewValue(newValue);
-                            }
-
-                            if (objNode.get("_source").get("OldValue") != null) {
-                                String oldValue = ("Old Value: " + objNode.get("_source").get("OldValue").asText());
-                                event.setNewValue(oldValue);
-                            }
-
-                            if (objNode.get("_source").get("CarType") != null) {
-                                String carType = ("Car Type: " + objNode.get("_source").get("CarType").asText());
-                                event.setNewValue(carType);
-                            }
-                            if (objNode.get("_source").get("Stage") != null) {
-                                String stage = ("Stage: " + objNode.get("_source").get("Stage").asText());
-                                event.setNewValue(stage);
-                            }
-
-                            eventList.add(event);
-
-                        }
-                    }
-
-                    Collections.sort(eventList, new Comparator<Event>() {
-                        public int compare(Event one, Event two) {
-                            Date thisTime = one.getDateTime();
-                            Date anotherTime = two.getDateTime();
-                            return thisTime.compareTo(anotherTime) < 0 ? -1 : (thisTime == anotherTime ? 0 : 1);
-                        }
-                    });
-
-String json = new Gson().toJson(eventList);
-return json;
-                });
 
 
             get("/event", (req, res) -> {
@@ -199,19 +117,21 @@ return json;
                     return thisTime.compareTo(anotherTime) < 0 ? -1 : (thisTime == anotherTime ? 0 : 1);
                 }});
 
+if(shouldReturnJson(req)){
+
+    String json = new Gson().toJson(eventList);
+    return json;
+}
+else {
+
+    model.put("template", "templates/event.vtl");
+
+    model.put("eventList", eventList);
+
+    return new VelocityTemplateEngine().render(new ModelAndView(model,"templates/layout.vtl" ));
+}  });}
 
 
-            model.put("template", "templates/event.vtl");
-
-            model.put("eventList", eventList);
-
-
-
-            return new ModelAndView(model, "templates/layout.vtl");
-        },new VelocityTemplateEngine());
-
-
-    }
 
     public static java.util.Date formatDate(String date) {
         Date response = new Date();
@@ -228,6 +148,11 @@ return json;
         return response;
     }
 
+
+    private static boolean shouldReturnJson(Request request) {
+        String accept = request.headers("Accept");
+        return accept != null && accept.contains("text/json");
+    }
 }
 
 
